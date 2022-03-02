21621d522c86af29a1ec1d5fcac739493fbb5c0e
Mehadyhasanakash <mehadyhasan1000@gmail.com>
2022-03-02 16:31:10 +0600

    commit

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const API_KEY = `efa3fe2cdd08d2c5cd0d0e48c1a701c6`;
+const apiKey = `efa3fe2cdd08d2c5cd0d0e48c1a701c6`;
 
 
 
 const search = ()=>{
     const input = document.getElementById('input_filed').value;
-    const url =`api.openweathermap.org/data/2.5/weather?q=${input}&appid=${API_KEY}`;
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=metric`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => temperature(data));
+}
+
+const sedTemperature =(id, text)=>{
+    document.getElementById(id).innerText= text;
+}
+
+const temperature = (hotTemperature)=>{
+    sedTemperature ('city', hotTemperature.name);
+    sedTemperature ('temperature', hotTemperature.main.temp);
 }
